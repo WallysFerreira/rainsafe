68a6fff829269ae7783d3a6d2ad40a71472a8ed7
Wallys Ferreira <wallysfsilva@gmail.com>
2024-11-30 17:44:48 -0300

    #16 arrange welcome page

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -5,7 +5,8 @@
 <style>
   a {
       display: block;
-      padding: 2vh 0;
+      padding-top: 0.6vh;
+      padding-bottom: 2vh;
       text-align: center;
       text-decoration: underline white;
   }
@@ -13,13 +14,39 @@
   a, p {
       font-size: 1.1em;
   }
+
+  #wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  #mainButtons{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #ssoButtons > :global(button) {
+    margin-top: 2vh;
+  }
 </style>
 
-<img src="/raincife.png" alt="Raincife logo" />
+<div id="wrapper">
+  <img src="/raincife.png" alt="Raincife logo" style="width: 144px;"/>
 
-<Button text="Cadastrar-se" fontSizeInEm=1.1 widthInVw=62.7 heightInVh=6.25 />
-<p>Já possui uma conta?</p>
-<a href="/welcome/login">Faça login</a>
+  <div id="mainButtons">
+    <Button text="Cadastrar-se" fontSizeInEm=1.1 widthInVw=62.7 heightInVh=6.25 />
+    <p style="margin-top: 2vh">Já possui uma conta?</p>
+    <a href="/welcome/login">Faça login</a>
+  </div>
 
-<Button ssoPlatform="Google" widthInVw=64 heightInVh=4 />
-<Button ssoPlatform="faceBook" widthInVw=64 heightInVh=4 />
+  <div id="ssoButtons">
+    <Button ssoPlatform="Google" widthInVw=64 heightInVh=4 />
+    <Button ssoPlatform="faceBook" widthInVw=64 heightInVh=4 />
+  </div>
+</div>
