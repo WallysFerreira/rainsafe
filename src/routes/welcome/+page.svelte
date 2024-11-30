5080e95df9f423fba06110161721b052d7e92633
Wallys Ferreira <wallysfsilva@gmail.com>
2024-11-30 16:18:57 -0300

    #16 add link to login and change sso button size

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -4,7 +4,11 @@
 
 <div>
   <h1>Logo RainSafe</h1>
+
   <Button text="Cadastrar-se" widthInVw=62.7 heightInVh=6.25 />
-  <Button ssoPlatform="Google"/>
-  <Button ssoPlatform="faceBook"/>
+  <p>Já possui uma conta?</p>
+  <a href="/welcome/login">Faça login</a>
+
+  <Button ssoPlatform="Google" widthInVw=64 heightInVh=4 />
+  <Button ssoPlatform="faceBook" widthInVw=64 heightInVh=4 />
 </div>
