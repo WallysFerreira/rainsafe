aff454f1fbc86ac358d324a7510c9cd066dc554f
Wallys Ferreira <wallysfsilva@gmail.com>
2024-11-30 14:27:41 -0300

    scaffold welcoming structure

---
+++ src/routes/welcome/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import '../../app.css'
+
+  let { children } = $props();
+</script>
+
+<style>
+  #welcomeBody {
+      background-color: var(--primary-color);
+      width: 100vw;
+      height: 100vh;
+    }
+</style>
+
+
+<div id="welcomeBody">
+  {@render children()}
+</div>
+
---
+++ src/routes/welcome/+page.svelte
@@ -0,0 +1,6 @@
+<div>
+<h1>Logo RainSafe</h1>
+<p>Botão cadastre-se</p>
+<p>Botão google</p>
+<p>Botão facebook</p>
+</div>
