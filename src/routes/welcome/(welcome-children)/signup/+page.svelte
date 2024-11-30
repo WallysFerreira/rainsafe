7be247afa551581340cf9c7ad2c8479c76a4f4e8
Wallys Ferreira <wallysfsilva@gmail.com>
2024-11-30 18:56:32 -0300

    build signup page skeleton

--- src/routes/welcome/(welcome-children)/signup/+page.svelte
+++ src/routes/welcome/(welcome-children)/signup/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Tela de cadastro</h1>
+<script>
+  import Button from '$lib/ui-components/welcome/Button.svelte';
+</script>
+
+<p>Email ou número de telefone</p>
+<input type="text" />
+
+<p>Senha</p>
+<input type="password" />
+
+<p>Confirme a senha</p>
+<input type="password" />
+
+<p>Data de nascimento</p>
+<input type="date" />
+
+<p>Localização</p>
+<select name="location" id="locationSelect">
+  <option value="loc1">Localizacao 1</option>
+</select>
+
+<Button text="Criar conta"/>
+
