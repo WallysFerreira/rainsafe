40de0f69f2888bd61c47804499a5e0b0dce906f0
Wallys Ferreira <wallysfsilva@gmail.com>
2024-11-30 22:15:20 -0300

    finish navbar

--- src/routes/welcome/(welcome-children)/+layout.svelte
+++ src/routes/welcome/(welcome-children)/+layout.svelte
@@ -2,9 +2,32 @@
   let { children } = $props();
 </script>
 
+<style>
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  button {
+    background: none;
+    border: none;
+  }
+
+  #raincifeLogo {
+    width: 66px;
+  }
+</style>
+
 <nav>
-  <h1>Botão pra voltar</h1>
-  <h1>Logo raincife</h1>
+  <button>
+    <img src="/back.png" alt="Button to go back" />
+  </button>
+
+  <img src="/raincife.png" alt="Raincife logo" id="raincifeLogo"/>
+
+  <div style="width: 10vw;"></div>
 </nav>
 
 {@render children()}
