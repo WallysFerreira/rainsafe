4aa9bdbfb8dceceb3352fd35724fda8270e4a8d4
Wallys Ferreira <wallysfsilva@gmail.com>
2024-12-02 10:00:14 -0300

    scaffold login page

--- src/routes/welcome/(welcome-children)/login/+page.svelte
+++ src/routes/welcome/(welcome-children)/login/+page.svelte
@@ -1,1 +1,38 @@
-<h1>Bem vindo ao RainSafe</h1>
+<script>
+  import Input from '$lib/ui-components/welcome/Input.svelte';
+  import Button from '$lib/ui-components/welcome/Button.svelte';
+</script>
+
+<style>
+  form {
+    width: 100%;
+    height: calc(30% - 66px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
+<h1 style="font-size: 1.5em">Bem vindo(a) ao RainSafe</h1>
+
+<form method="POST">
+  <label>
+    Email
+    <Input name="email" type="email" style="width: 75vw;" required />
+  </label>
+
+  <label>
+    Senha
+    <Input name="password" type="password" minlength="6" style="width: 75vw;" required />
+  </label>
+
+  <Button text="Entrar" />
+</form>
+
+<div style="height: 15vh;"></div>
